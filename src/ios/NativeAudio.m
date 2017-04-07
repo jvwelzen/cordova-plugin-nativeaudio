d9af74ca2dbeabe7358eca220b44f20bfe2ddc4a
jvwelzen <info@hws-koeriers.nl>
2017-04-07 13:57:18 +0200

    Update NativeAudio.m

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -193,7 +193,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     NSString *callbackId = command.callbackId;
     NSArray* arguments = command.arguments;
     NSString *audioID = [arguments objectAtIndex:0];
-    NSString *audioType = [arguments objectAtIndex:1]; 
+    audioType = '1'; 
     
         if(audioType == '1'){ // AudioType == Duck Others
             
@@ -265,7 +265,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     NSString *callbackId = command.callbackId;
     NSArray* arguments = command.arguments;
     NSString *audioID = [arguments objectAtIndex:0];
-    NSString *audioType = [arguments objectAtIndex:1]; 
+    audioType = '1';
     
         if(audioType == '1'){ // AudioType == Duck Others
             
